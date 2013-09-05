c4583d8e1bb0f74be8a80ec328d8ad9ce11ee7de
monilip <monilip@onet.eu>
2013-09-05 21:13:34 +0200

    Nowe litery

--- Text-recognition/main.cpp
+++ Text-recognition/main.cpp
@@ -1,7 +1,5 @@
 #include "app.h"
 #include "LetterAnalyzer.h"
-#include "TrainingSet.h"
-#include "Kohonen.h"
 #include <QtWidgets/QApplication>
 #include <qimage.h>
 #include <qimagereader.h>
@@ -62,11 +60,12 @@ void extractContours(Mat& image,vector< vector<Point> > contours_poly)
         image.copyTo(extractPic,mask);
         Mat resizedPic = extractPic(r);
        
-        Mat image=resizedPic.clone();
- 
+        bitwise_not(resizedPic, resizedPic); //kontrast bo cv u¿ywa czarnego jako t³a
         //zapisywanie
 		stringstream file;
-        file<<"letters/"<<i<<".jpg";
+		
+		char ch = (char)(i+65); 
+        file<<"letters/"<<ch<<5<<".jpg";
         imwrite(file.str(),resizedPic);
 
  
@@ -79,13 +78,13 @@ void image()
 	//problem z plikami o ma³ej rozdzielczoœci!
 
 
-	Mat img = imread("img/image3.jpg",0);  //dodaæ zabezpieczenia: co jak nie siê plik Ÿle otworzy
+	Mat img = imread("img/Bookman_Old_Style.jpg",0);  //dodaæ zabezpieczenia: co jak nie siê plik Ÿle otworzy
 
 	//zamiana na czarno-bia³y
 	Size size(3,3); 
 	GaussianBlur(img,img,size,0); //zamglenie
     adaptiveThreshold(img, img,255,CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY,75,10); //progowanie
-    //bitwise_not(img, img); //kontrast bo cv u¿ywa czarnego jako t³a
+    bitwise_not(img, img); //kontrast bo cv u¿ywa czarnego jako t³a
  
 	//kopia
 	Mat img2 = img.clone();
@@ -187,7 +186,7 @@ void image()
     }
  
  
-    //wyœwirtlanie konturów
+    //wyœwietlanie konturów
 	Scalar color = Scalar(0,255,0);
 	for( int i = 0; i< validContours.size(); i++ )
 		{
@@ -198,112 +197,20 @@ void image()
 		}
  
 	extractContours(cropped3,validContours);
+	imshow("title",cropped2);
 
-	imshow("test", cropped3);
 }
 
 int main(int argc, char *argv[])
 {
-	//image();
-	QString letters[26] = {"training-sets/A1.jpg", "training-sets/B1.jpg", "training-sets/C1.jpg", "training-sets/D1.jpg",
-						   "training-sets/E1.jpg", "training-sets/F1.jpg", "training-sets/G1.jpg", "training-sets/H1.jpg",
-						   "training-sets/I1.jpg", "training-sets/J1.jpg", "training-sets/K1.jpg", "training-sets/L1.jpg",
-						   "training-sets/M1.jpg", "training-sets/N1.jpg", "training-sets/O1.jpg", "training-sets/P1.jpg",
-						   "training-sets/Q1.jpg", "training-sets/R1.jpg", "training-sets/S1.jpg", "training-sets/T1.jpg",
-						   "training-sets/U1.jpg", "training-sets/V1.jpg", "training-sets/W1.jpg", "training-sets/X1.jpg",
-						   "training-sets/Y1.jpg", "training-sets/Z1.jpg"};
-
-	QString letters2[26] = {"training-sets/A2.jpg", "training-sets/B2.jpg", "training-sets/C2.jpg", "training-sets/D2.jpg",
-						   "training-sets/E2.jpg", "training-sets/F2.jpg", "training-sets/G2.jpg", "training-sets/H2.jpg",
-						   "training-sets/I2.jpg", "training-sets/J2.jpg", "training-sets/K2.jpg", "training-sets/L2.jpg",
-						   "training-sets/M2.jpg", "training-sets/N2.jpg", "training-sets/O2.jpg", "training-sets/P2.jpg",
-						   "training-sets/Q2.jpg", "training-sets/R2.jpg", "training-sets/S2.jpg", "training-sets/T2.jpg",
-						   "training-sets/U2.jpg", "training-sets/V2.jpg", "training-sets/W2.jpg", "training-sets/X2.jpg",
-						   "training-sets/Y2.jpg", "training-sets/Z2.jpg"};
-
-	QString letters3[26] = {"training-sets/A3.jpg", "training-sets/B3.jpg", "training-sets/C3.jpg", "training-sets/D3.jpg",
-						   "training-sets/E3.jpg", "training-sets/F3.jpg", "training-sets/G3.jpg", "training-sets/H3.jpg",
-						   "training-sets/I3.jpg", "training-sets/J3.jpg", "training-sets/K3.jpg", "training-sets/L3.jpg",
-						   "training-sets/M3.jpg", "training-sets/N3.jpg", "training-sets/O3.jpg", "training-sets/P3.jpg",
-						   "training-sets/Q3.jpg", "training-sets/R3.jpg", "training-sets/S3.jpg", "training-sets/T3.jpg",
-						   "training-sets/U3.jpg", "training-sets/V3.jpg", "training-sets/W3.jpg", "training-sets/X3.jpg",
-						   "training-sets/Y3.jpg", "training-sets/Z3.jpg"};
-
-	QString letters4[26] = {"training-sets/A4.jpg", "training-sets/B4.jpg", "training-sets/C4.jpg", "training-sets/D4.jpg",
-						   "training-sets/E4.jpg", "training-sets/F4.jpg", "training-sets/G4.jpg", "training-sets/H4.jpg",
-						   "training-sets/I4.jpg", "training-sets/J4.jpg", "training-sets/K4.jpg", "training-sets/L4.jpg",
-						   "training-sets/M4.jpg", "training-sets/N4.jpg", "training-sets/O4.jpg", "training-sets/P4.jpg",
-						   "training-sets/Q4.jpg", "training-sets/R4.jpg", "training-sets/S4.jpg", "training-sets/T4.jpg",
-						   "training-sets/U4.jpg", "training-sets/V4.jpg", "training-sets/W4.jpg", "training-sets/X4.jpg",
-						   "training-sets/Y4.jpg", "training-sets/Z4.jpg"};
-
+	image();
 	QImage img = QImage();
-
-	TrainingSet set = TrainingSet();
-	TrainingSet set2 = TrainingSet();
-	TrainingSet set3 = TrainingSet();
-	TrainingSet set4 = TrainingSet();
-	
-	int j = 0;
-
-	for (int i = 0; i < 26; i++)
-	{
-		if (img.load(letters[i]))
-		{
-			QImage cropped = LetterAnalyzer::crop(img);
-			//cropped.save(letters[i]);
-			double* analyzed = LetterAnalyzer::parse(cropped);
-			set.insertSet(i, analyzed);
-			//j++;
-		}
-
-		if (img.load(letters2[i]))
-		{
-			QImage cropped = LetterAnalyzer::crop(img);
-			//cropped.save(letters[i]);
-			set2.insertSet(i, LetterAnalyzer::parse(cropped));
-			//j++;
-		}
-
-		if (img.load(letters3[i]))
-		{
-			QImage cropped = LetterAnalyzer::crop(img);
-			//cropped.save(letters[i]);
-			set3.insertSet(i, LetterAnalyzer::parse(cropped));
-		//	j++;
-		}
-
-		if (img.load(letters4[i]))
-		{
-			QImage cropped = LetterAnalyzer::crop(img);
-			//cropped.save(letters[i]);
-			set4.insertSet(i, LetterAnalyzer::parse(cropped));
-		//	j++;
-		}
-	}
-
-/*	TrainingSet trainingSets[4];
-	trainingSets[0] = set;
-	trainingSets[1] = set2;
-	trainingSets[2] = set3;
-	trainingSets[3] = set4;*/
-
-	Kohonen network = Kohonen();
-/*	network.init();*/
-	network.learn(&set);
-/*	network.learn(&set2);
-	network.learn(&set3);
-	network.learn(&set4);*/
-	double normalizationFactor = 0.0;
-	if (img.load("training-sets/P4.jpg"))
+	if (img.load("training-sets/Y2.jpg"))
 	{
 		QImage cropped = LetterAnalyzer::crop(img);
-		set.insertSet(0, LetterAnalyzer::parse(cropped));
+		cropped.save("training-sets/testY2.jpg");
+		LetterAnalyzer::parse(cropped);
 	}
-
-	int winner = network.pickWinner(&set, 0, normalizationFactor);
-	int letter = network.getMap()[winner];
-
 	QApplication a(argc, argv);
 	App w;
 	w.show();
